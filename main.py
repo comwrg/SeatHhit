cbfc765c7523c166442c300bcce59456ddd3ae30
comwrg <xcomwrg@gmail.com>
2018-03-13 14:31:03 +0800

    fix date weekday

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ roomname = '西104'
 seat_number = '116'
 date = next_day.strftime('%Y-%m-%d')
 
-if datetime.now().isoweekday() is not 3:
+if next_day.isoweekday() is not 3:
     # not wednesday
     r = s.set_seat(labname, roomname, seat_number, date, '08:00', '21:00')
     print(r)
